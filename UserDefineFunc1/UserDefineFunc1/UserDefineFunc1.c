5ee5cbbab75d67437f0c8e586e98cc0b266d1d77
subinmun1997 <qzxy812@gmail.com>
2020-07-19 13:49:34 +0900

    UserDefinedFunc

---
+++ UserDefineFunc1/UserDefineFunc1/UserDefineFunc1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void func(int i, int j);
+
+int main(void) {
+	int a = 3, b = 12;
+	func(a, b);
+	printf("%d, %d\n", a, b);
+}
+
+void func(i, j)
+
+int i, j;
+{
+	i *= 3;
+	j /= 3;
+	printf("%d, %d\n", i, j);
+}
---
+++ UserDefineFunc2/UserDefineFunc2/UserDefineFunc2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+func(i, j);
+
+int main(void) {
+	int a = 3, b = 12;
+	a = func(a, b);
+	
+	printf("%d, %d\n", a, b);
+}
+func(i, j)
+
+int i, j;
+{
+	i *= 3;
+	j /= 3;
+	printf("%d, %d\n", i, j);
+	return i;
+}
